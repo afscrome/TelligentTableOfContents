7bcda2ca642e5b0ca65de135c7ef55cba4b5d0fb
Alex Crome <afscrome@users.noreply.github.com>
2011-08-07 13:59:52 +0000

    Updated Version number to 6.0.x

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("5.6.*")]
+[assembly: AssemblyVersion("6.0.*")]
 [assembly: AssemblyCompany("Telligent")]
 [assembly: AssemblyCopyright("Copyright © Telligent 2010")]
 [assembly: AssemblyProduct("Telligent.Evolution.Extensions.TableOfContents")]
