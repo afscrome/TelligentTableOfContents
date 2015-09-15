9b9e06d445cf93991993ec33413d3b50202a14c7
Alex Crome <afscrome@users.noreply.github.com>
2015-09-15 23:14:43 +0100

    Added assembly description.

--- TableOfContents/Properties/AssemblyInfo.cs
+++ TableOfContents/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("AlexCrome.Telligent.TableOfContents")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Table of Contents extension for Telligent Community")]
 #if DEBUG
 [assembly: InternalsVisibleTo("AlexCrome.Telligent.TableOfContents.Tests")]
 #endif
