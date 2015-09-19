6c8aa70ef0c884f548eda47fcb00acdad85e9fe4
Alex Crome <afscrome@users.noreply.github.com>
2015-09-19 15:41:30 +0100

    Updating assembly version attributes

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -2,10 +2,13 @@ using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("7.0.*")]
+[assembly: AssemblyVersion("0.0.*")]
+[assembly: AssemblyFileVersion("0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0-alpha1")]
+
 [assembly: AssemblyCompany("Telligent")]
 [assembly: AssemblyCopyright("Copyright © Telligent 2010")]
 [assembly: AssemblyProduct("AlexCrome.Telligent.TableOfContents")]
 
 [assembly: ComVisible(false)]
-[assembly:CLSCompliant(false)]
+[assembly: CLSCompliant(false)]
